1a952079b96f098bbe444128a9edfcce7242c2f4
rinstance <30986957+rinstance@users.noreply.github.com>
2023-11-19 14:16:47 +0300

    add module paths in buildSrc

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,17 @@
+object Modules {
+    //region core
+    const val coreFeatureApi = ":core:feature_api"
+    const val coreNavigation = ":core:navigation"
+    const val coreUi = ":core:ui"
+    const val coreUtils = ":core:utils"
+    //endregion
+
+    //region auth
+    const val authApi = ":feature:auth:api"
+    const val authPresentation = ":feature:auth:presentation"
+    //endregion
+
+    //region home
+    const val homeApi = ":feature:home:api"
+    //endregion
+}
